359961444f9425ffb6323850f2438827963f78cc
Tkazer <1319923129@qq.com>
2023-10-01 18:58:20 +0800

    [Delete] Deleted debug code.

--- CS2_External/Cheats.cpp
+++ CS2_External/Cheats.cpp
@@ -96,8 +96,6 @@ void Cheats::Run()
 		Gui.Text(Entity.Pawn.WeaponName, { Rect.x,Rect.y + Rect.w }, ImColor(255, 255, 255, 255), 17);
 	}
 
-	//AimControl::RecoilControl(LocalEntity);
-
 	if (GetAsyncKeyState(AimControl::HotKey))
 	{
 		if (AimPos != Vec3(0,0,0))
